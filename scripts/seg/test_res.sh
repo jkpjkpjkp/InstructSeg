2d2b5d183a55916765be772683b724603021f199
congvvc <115617400+congvvc@users.noreply.github.com>
2024-12-20 16:13:54 +0800

    InstructSeg v1

--- scripts/seg/test_res.sh
+++ scripts/seg/test_res.sh
@@ -4,7 +4,7 @@
 
 deepspeed --master_port=28998 instructseg/eval/seg/eval_res.py \
     --image_folder dataset/coco/train2014 \
-    --json_path dataset/refcoco/refcoco_evaluation.json \
+    --json_path dataset/RES/refcoco/refcoco_val.json \
     --mask_config instructseg/model/mask_decoder/mask_config/maskformer2_swin_base_384_bs16_50ep.yaml \
     --model_path model/InstructSeg \
     --output_dir output/res \
